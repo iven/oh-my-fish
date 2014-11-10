10d0cf943fefac30426de348588cefc85a6008bf
Xu Lijian <xulijian@qiyi.com>
2014-11-10 14:57:08 +0800

    Add enable and disable for systemd plugin

--- themes/jacaetevha/fish_prompt.fish
+++ themes/jacaetevha/fish_prompt.fish
@@ -1,7 +1,7 @@
 # name: Jacaetevha
 function tbytes -d 'calculates the total size of the files in the current directory'
   set -l tBytes (ls -al | grep "^-" | awk 'BEGIN {i=0} { i += $5 } END { print i }')
-  
+
   if test $tBytes -lt 1048576
     set -g total (echo -e "scale=3 \n$tBytes/1048 \nquit" | bc)
     set -g units " Kb"
@@ -13,29 +13,29 @@ function tbytes -d 'calculates the total size of the files in the current direct
 end
 
 function fish_prompt
-  set_color yellow
-  printf '%s' (whoami)
-  set_color normal
-  printf ' at '
+  #set_color yellow
+  #printf '%s' (whoami)
+  #set_color normal
+  #printf ' at '
 
-  set_color magenta
-  printf '%s' (hostname|cut -d . -f 1)
-  set_color normal
-  printf ' in '
+  #set_color magenta
+  #printf '%s' (hostname|cut -d . -f 1)
+  #set_color normal
+  #printf ' in '
 
   set_color $fish_color_cwd
-  printf '%s ' (prompt_pwd)
-  set_color blue
-  printf '%s' (tbytes)
+  printf '%s' (prompt_pwd)
+  #set_color blue
+  #printf '%s' (tbytes)
   set_color normal
-  printf '%s' (__fish_git_prompt)
+  printf '%s ' (__fish_git_prompt)
 
   # Line 2
-  echo
+  #echo
   if test $VIRTUAL_ENV
       printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
   end
-  printf '↪  '
+printf '⇨  '
   set_color normal
 end
 
